cbb7282505ba2d216cfed631d9a5e90f27355b5f
Lyre <lvguoqing@daojia.com>
2018-06-11 11:14:03 +0800

    init code -- LYRE

---
+++ src/main/java/com/lyre/demo/ApplicationStartup.java
@@ -0,0 +1,15 @@
+package com.lyre.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by LV on 2018/6/11.
+ */
+@SpringBootApplication
+public class ApplicationStartup {
+
+        public static void main(String[] args) {
+            SpringApplication.run(ApplicationStartup.class, args);
+    }
+}
